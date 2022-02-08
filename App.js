1d06902f02b549cbb2c90cd0cb888dfd9ad3cb79
Edmundo roberto <edmundomarinho50@hotmail.com>
2022-02-08 17:18:28 -0300

    ReactNative start

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { View, Text,} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+class App extends Component{
+
+
+  
+     render(){
+       
+     return(
+
+      <View style={{marginTop: 40,flex:1, alignItems:'center', justifyContent: 'center', backgroundColor: 'orange'}}>
+        <Text style={{fontSize: 30, color: '#fff'}}>
+          Hellow World 
+        </Text>
+      </View>
+      
+      
+
+     );      
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+
+export default App;
+
+
+ 
+
+
